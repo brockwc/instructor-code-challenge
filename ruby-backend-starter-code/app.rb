0a29eb66dabe3e3bb9a833703e8c82a2ec8fd100
zap-rowsdower <brock.whitbread@gmail.com>
2018-05-18 10:41:54 -0700

    refactoring back to original backend to find problem (problem found!)

--- ruby-backend-starter-code/app.rb
+++ ruby-backend-starter-code/app.rb
@@ -12,18 +12,15 @@ end
 
 post '/favorites' do
   file = JSON.parse(File.read('data.json'))
-  
+
   unless params[:name] && params[:oid]
     return 'Invalid Request'
   end
+  
+  movie = { name: params[:name], oid: params[:oid] }
+  file << movie
+  File.write('data.json',JSON.pretty_generate(file))
+  movie.to_json
 
-  film = { name: params[:name], oid: params[:oid] }
-  file = file.merge(film)
-
-  File.open('data.json', 'w') do |f|
-    f.write(JSON.pretty_generate(file))
-  end
-
-  # File.write('data.json', JSON.pretty_generate(file))
-  # film.to_json
+  redirect '/'
 end
