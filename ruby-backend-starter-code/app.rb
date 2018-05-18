f39a525e40f68cb081146eda07bf33ff5d427bc1
zap-rowsdower <brock.whitbread@gmail.com>
2018-05-17 17:26:19 -0700

    save 'favorite' films to data.json

--- ruby-backend-starter-code/app.rb
+++ ruby-backend-starter-code/app.rb
@@ -5,18 +5,25 @@ get '/' do
   File.read('views/index.html')
 end
 
-get 'favorites' do
+get '/favorites' do
   response.header['Content-Type'] = 'application/json'
   File.read('data.json')
 end
 
 post '/favorites' do
   file = JSON.parse(File.read('data.json'))
+  
   unless params[:name] && params[:oid]
     return 'Invalid Request'
   end
-  movie = { name: params[:name], oid: params[:oid] }
-  movie.to_json
-  file << movie
-  File.write('data.json', JSON.pretty_generate(file))
+
+  film = { name: params[:name], oid: params[:oid] }
+  file = file.merge(film)
+
+  File.open('data.json', 'w') do |f|
+    f.write(JSON.pretty_generate(file))
+  end
+
+  # File.write('data.json', JSON.pretty_generate(file))
+  # film.to_json
 end
